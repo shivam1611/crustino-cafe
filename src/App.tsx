1fe9381f9cff75cb16b22c2ca217a1792c2c5d4d
codeshey <iamshivamsharma755@gmail.com>
2024-10-10 16:22:18 +0530

    initial app

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 
 const App = () => {
   return (
-    <div className="text-white bg-green-500 p-2 text-2xl text-center">App</div>
+    <div className="text-white bg-green-500 p-2 text-2xl text-center">App is initialized fine</div>
   )
 }
 
